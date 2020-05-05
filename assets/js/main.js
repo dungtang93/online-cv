801b979839f2d87a33d1af57daf00bb098416113
dungtang93 <dungtang93@gmail.com>
2020-05-05 21:58:49 +0700

    animate level bar only when scrolled to

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
 jQuery(document).ready(function($) {
 
     $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
 
-        $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-            
-        });
+    // animate the level bars when scrolled to
+    $(window).scroll(function() {
+        var hT = $('.level-bar').offset().top,
+            hH = $('.level-bar').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
 
+        if (wS > (hT + hH - wH)) {
+            $('.level-bar-inner').each(function() {
+                var itemWidth = $(this).data('level');
+                $(this).animate({
+                    width: itemWidth
+                }, 800);
+            });
+        }
     });
-   
-    
-
 });
