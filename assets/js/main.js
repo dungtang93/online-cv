10a3442777e4ecd0ce7522553814e24bcf8943e0
dungtang93 <dungtang93@gmail.com>
2020-05-29 14:46:04 +0700

    IE and edge support for proper printing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,24 @@
 jQuery(document).ready(function($) {
+    if ($('.level-bar').length > 0) {
+        $('.level-bar-inner').css('width', '0');
 
-    $('.level-bar-inner').css('width', '0');
-
-    // animate the level bars when scrolled to
-    $(window).scroll(function() {
-        var hT = $('.level-bar').offset().top,
-            hH = $('.level-bar').outerHeight(),
-            wH = $(window).height(),
-            wS = $(this).scrollTop();
-
-        if (wS > (hT + hH - wH)) {
-            $('.level-bar-inner').each(function() {
-                var itemWidth = $(this).data('level');
-                $(this).animate({
-                    width: itemWidth
-                }, 800);
-            });
-        }
-    });
+        // animate the level bars when scrolled to
+        $(window).scroll(function() {
+            var hT = $('.level-bar').offset().top,
+                hH = $('.level-bar').outerHeight(),
+                wH = $(window).height(),
+                wS = $(this).scrollTop();
+    
+            if (wS > (hT + hH - wH)) {
+                $('.level-bar-inner').each(function() {
+                    var itemWidth = $(this).data('level');
+                    $(this).animate({
+                        width: itemWidth
+                    }, 800);
+                });
+            }
+        });
+    }
 });
 
 // skip animation when print to show the level bars
